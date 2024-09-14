c6df70c69d043272370a5fd0c054c831874554d1
HowardWei123 <howieweiinf@gmail.com>
2024-09-14 03:09:20 -0700

    components

--- SGWebsite/src/App.vue
+++ SGWebsite/src/App.vue
@@ -1,5 +1,25 @@
 <template>
-  <header>
-    Spartan Gaming
-  </header>
+  <div id="app">
+    <Header />
+    <router-view />
+    <Footer />
+  </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Header from '../src/components/Header.vue';
+import Footer from '../src/components/Footer.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    Header,
+    Footer,
+  },
+});
+</script>
+
+<style>
+/* Global styles */
+</style>
---
+++ SGWebsite/src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer class="bg-gray-800 text-white text-center p-4">
+    <p class="text-sm">
+      <a href="#" class="hover:text-blue-400">Community Guidelines</a> |
+        <span>Copyright © Spartan Gaming 2024</span> |
+        <span>Web Design by Howard Wei</span>
+    </p>
+  </footer>
+</template>
+  
+<script lang="ts">
+  import { defineComponent } from 'vue';
+  
+  export default defineComponent({
+    name: 'Footer',
+  });
+</script>
